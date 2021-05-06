05a01a3d666a99e0da94b8adc0e24f057cbf0c4a
onemo <wonmotime1@naver.ccom>
2021-05-06 14:01:30 +0900

    [ADD] first connect to mongodb

---
+++ config/database.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const { databaseURI, databasePassword } = require('./index');
+
+const connectMongoose = async () => {
+  try {
+    await mongoose.connect(
+      databaseURI.replace('<password>', databasePassword),
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+        dbName: 'bits',
+      }
+    );
+
+    console.log('Successfully connected to mongodb');
+  } catch (error) {
+    console.error(`Error on connect to mongodb ${error}`);
+  }
+};
+
+connectMongoose();
---
+++ config/index.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+  databaseURI: process.env.DATABASE_URI,
+  databasePassword: process.env.DATABASE_PASSWORD
+};
